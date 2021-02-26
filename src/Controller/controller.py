afbc48188ecbe265b7aee59318687039dd0fa4a4
FernandoItami <fernandoitami@gmail.com>
2021-02-27 06:20:32 +0900

    controller update

--- src/Controller/controller.py
+++ src/Controller/controller.py
@@ -1,5 +1,12 @@
 import requests
+import json
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
 resp = requests.get('https://api.openbrewerydb.org/breweries')
 
-print(resp.json())
+for item in resp.json():
+    print(item.get('name'))
